ff94a154d750ff9721c10af360fdc322ca7b8e85
Alexandra Corremans <alexandra.corremans@gmail.com>
2018-12-09 12:58:44 +1100

    finished module InputParser (for now) - can now parse most variations of input strings into a piece type and endpoint - realized entering a piece type and endpoint doesn't always uniquely identify a move on a chess board. The idea is to accommodate this rather than to ask the player to input a uniquely identifying move. To provide for these ambiguous situations, there would need to be a way for the board to return a list of valid moves and to ask the player to confirm which one it is. If that turns out to be too hard, I'll need to revise the kind of input required and parsed, but I'd like to try first

--- lib/inputparser.rb
+++ lib/inputparser.rb
@@ -14,9 +14,9 @@ module InputParser
         choice[:type] = 'other'
         choice[:contents] = input
         return choice
-      elsif input.match(regexp)
+      elsif !move_parse(input).nil?
         choice[:type] = 'move'
-        choice[:contents] = analyse(regexp)
+        choice[:contents] = move_parse(input)
         return choice
       else
         puts "Invalid input, please try again or type 'help' if you don't know what to do:"
@@ -25,14 +25,67 @@ module InputParser
     end
   end
 
+  def has_coordinates?(input)
+    input =~ regexp
+  end
+
+  def move_parse(input)
+    return nil unless has_coordinates?(input)
+    endpoint = get_endpoint(input)
+    piece_type = get_piece(input)
+    return nil if piece_type.nil?
+    { 
+      endpoint: endpoint, 
+      piece_type: piece_type
+    }
+  end
+
+  def get_endpoint(input)
+    coordinates = input[regexp].split(/\s?/)
+    endpoint = convert(coordinates)
+  end
+
+  def get_piece(input)
+    input = input.sub(input[regexp], '').sub(/to/, '').strip
+    if pieces.include?(input)
+      return input
+    elsif input == ''
+      return 'pawn'
+    else
+      return nil
+    end
+  end
+
+  def convert(coordinates)
+    x = coordinates[0]
+    y = coordinates[1]
+    x = translations[x.downcase]
+    y = y.to_i - 1
+    [x,y]
+  end
+
   def options
     %w{help save quit resign}
   end
-
+  
   def regexp
+    /[a-hA-H]\s?[1-8]/
   end
 
-  def analyse(regexp)
-    # return a hash with piece type and end point
+  def pieces
+    %w{pawn rook knight bishop queen king}
+  end
+
+  def translations
+    {
+      'a' => 0,
+      'b' => 1,
+      'c' => 2,
+      'd' => 3,
+      'e' => 4,
+      'f' => 5,
+      'g' => 6,
+      'h' => 7
+    }
   end
 end
