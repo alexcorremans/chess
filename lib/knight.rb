bb6ca42bcdc4650b4bde5cbff57ca4550c78a874
Alexandra Corremans <alexandra.corremans@gmail.com>
2018-12-10 11:50:40 +1100

    idea for refactoring pieces added to knight.rb

--- lib/knight.rb
+++ lib/knight.rb
@@ -6,6 +6,13 @@ class Knight < Piece
     start = Square.new(x: a[0], y: a[1])
     path = [start]
     direction.each do |step|
+      current_square = start.generate_child(step)
+=begin
+      if current_square.x == b[0] && current_square.y == b[1]
+        path << current_square
+        return path
+      end
+=end
       if start.x + step[0] == b[0] && start.y + step[1] == b[1]
         path << Square.new(x: b[0], y: b[1], parent: start)
         return path
